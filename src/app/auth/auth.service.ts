23a31fc87c3a4f65a29b60dae57c4483bf3081e2
Sarah Facci <sarah.e.facci@gmail.com>
2017-05-09 15:15:56 -0400

    restored anonymized auth service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,59 @@
+import * as firebase from 'firebase';
+
+export class AuthService {
+  private userIsSignedIn = false;
+  private token = '';
+  private apiKey = 'your API Key here';
+  private authDomain = 'yourDBname.firebaseapp.com';
+
+
+  // required before making any further authentication attempts
+  intializeApp() {
+    firebase.initializeApp({ apiKey: this.apiKey , authDomain: this.authDomain } ); 
+  }
+ 
+  // sign in
+  signInUser(email: string, password: string) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then( 
+       response => { 
+         this.userIsSignedIn = true;
+         firebase.auth().currentUser.getToken()
+            .then(
+              (token: string) => this.token = token
+            )  
+       }
+      )
+      .catch(
+      (error: Error) => {console.log(error.message);}
+    );
+    
+  }
+
+  // sign out
+  signOutUser() {
+   firebase.auth().signOut().then().catch(); 
+    this.userIsSignedIn = false;
+  }
+
+  // update stored token and return to calling method
+  // need to look into the proper way to refresh tokens
+  getToken() {
+    firebase.auth().currentUser.getToken()
+      .then(
+        (token: string) => this.token = token
+      );
+    return this.token;
+  }
+
+  // may return before token is updated
+  isUserSignedIn() {
+    firebase.auth().onAuthStateChanged( 
+      user => { this.userIsSignedIn = user ? true : false;}
+    );
+    return this.userIsSignedIn;
+  }
+
+ 
+  
+} 
